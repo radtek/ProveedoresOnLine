04cdb4f71ef49edfdff5bf12eaa7520e1ae7e186
DFZ-23 <diego.jaramillo@publicar.com>
2016-12-05 17:23:23 -0500

    reporte todas las evaluaciones

--- ProveedoresOnLine.Reports/ProveedoresOnLine.Reports.Test/ReportTest.cs
+++ ProveedoresOnLine.Reports/ProveedoresOnLine.Reports.Test/ReportTest.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Threading.Tasks;
+using ProveedoresOnLine.Company.Models.Util;
+using ProveedoresOnLine.SurveyModule.Models;
 
 namespace ProveedoresOnLine.Reports.Test
 {
@@ -39,12 +41,57 @@ namespace ProveedoresOnLine.Reports.Test
         }
 
         [TestMethod]
-        public void SV_Report_SurveyGetAllByCustomer()
+        public void SV_Report_AllSurveyByCustomer()
         {
-            List<SurveyModule.Models.SurveyModel> oModel = ProveedoresOnLine.Reports.Controller.ReportModule.SurveyGetAllByCustomer("DA5C572E");
+            List<SurveyModule.Models.SurveyModel> oSurveyParents = SurveyModule.Controller.SurveyModule.SurveyGetAllByCustomer("1EA5A78A");
+            List<Tuple<SurveyModel, List<GenericItemModel>, List<GenericItemModel>, List<GenericItemModel>>> oReturn = new List<Tuple<SurveyModel, List<GenericItemModel>, List<GenericItemModel>, List<GenericItemModel>>>();
+            
+            oSurveyParents.All(x =>
+            {
+            List<Tuple<SurveyModel, List<GenericItemModel>, List<GenericItemModel>, List<GenericItemModel>>> oChildReturn = new List<Tuple<SurveyModel, List<GenericItemModel>, List<GenericItemModel>, List<GenericItemModel>>>();
+                List<SurveyModule.Models.SurveyModel> ChildSurvey = new List<SurveyModule.Models.SurveyModel>();
+                ChildSurvey.AddRange(SurveyModule.Controller.SurveyModule.ReportAllSurvey(Convert.ToString(x.SurveyPublicId), "1EA5A78A"));
+                    //TODO: Build obj tuple with parent and sons
 
+                List<GenericItemModel> Areas = new List<GenericItemModel>();
+                List<GenericItemModel> Questions = new List<GenericItemModel>();
+                List<GenericItemModel> Answers = new List<GenericItemModel>();
 
-            Assert.IsTrue(oModel != null);
+                ChildSurvey.All(m =>
+                {
+                    Areas.Add(m.RelatedSurveyConfig.RelatedSurveyConfigItem.Where(y => y.ItemType.ItemId == 1202001 && y.ParentItem == null).Select(z => z).FirstOrDefault());
+
+                    Areas.All(ar =>
+                    {
+                        Questions.AddRange(m.RelatedSurveyConfig.RelatedSurveyConfigItem.Where(e => e.ItemType.ItemId == 1202002 && e.ParentItem != null && e.ParentItem.ItemId == ar.ItemId).Select(e => e).ToList());
+                        return true;
+                    });
+                    Questions.All(q =>
+                    {
+                        Answers.Add(m.RelatedSurveyItem.Where(e => e != null && e.RelatedSurveyConfigItem != null && e.RelatedSurveyConfigItem.ItemId == q.ItemId).Select(e => new GenericItemModel()
+                        {
+                            ItemId = e.ItemId,
+                            ItemInfo = e.ItemInfo,
+                            CreateDate = x.CreateDate,
+                            ItemName = m.RelatedSurveyConfig.RelatedSurveyConfigItem.
+                                                                                Where(inf => inf.ItemId == int.Parse(e.ItemInfo.Where(subinf => subinf.ItemInfoType.ItemId == 1205003).
+                                                                                       Select(subinf => subinf.Value).FirstOrDefault())).Select(inf => inf.ItemName).FirstOrDefault(),
+                            ParentItem = new GenericItemModel()
+                            {
+                                ItemId = e.RelatedSurveyConfigItem.ItemId
+                            }
+                        }).FirstOrDefault());
+                        return true;
+                    });
+                    return true;
+                });
+                                
+                oChildReturn.Add(new Tuple<SurveyModel, List<GenericItemModel>, List<GenericItemModel>, List<GenericItemModel>>(x, Areas, Questions, Answers));
+                oReturn.AddRange(oChildReturn);
+                return true;
+            });
+            
+            Assert.IsTrue(oSurveyParents != null);
         }
 
         #endregion
@@ -62,13 +109,13 @@ namespace ProveedoresOnLine.Reports.Test
             DataTable data3 = new DataTable();
             DataTable data4 = new DataTable();
 
-            Tuple<byte[], string, string> report = 
-                ProveedoresOnLine.Reports.Controller.ReportModule.CP_GerencialReport("PDF", 
-                                                                                     data, 
+            Tuple<byte[], string, string> report =
+                ProveedoresOnLine.Reports.Controller.ReportModule.CP_GerencialReport("PDF",
+                                                                                     data,
                                                                                      data2,
                                                                                      data3,
                                                                                      data4,
-                                                                                     parameters, 
+                                                                                     parameters,
                                                                                      "C:\\Publicar Software\\ProveedoresOnLine\\ProveedoresOnLine.Reports\\ProveedoresOnLine.Reports.Test\\Reports\\C_Report_GerencialInfo.rdlc");
             parameters = null;
         }
@@ -118,7 +165,7 @@ namespace ProveedoresOnLine.Reports.Test
             rowProvider["financiera"] = "3 % no pasa Aprobado";
             rowProvider["legal"] = "pasa Aprobado";
             rowProvider["estado"] = "Ajudicado";
-            
+
             dtProvidersProject.Rows.Add(rowProvider);
 
             Tuple<byte[], string, string> report = ProveedoresOnLine.Reports.Controller.ReportModule.PJ_SelectionProcessReport(dtProvidersProject, parameters, "PDF", @"C:\PublicarPO\ProveedoresOnLine.Reports\ProveedoresOnLine.Reports\Reports\");
