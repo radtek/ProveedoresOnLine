1bbdbf067946cf6f8043f3be78e1e9e7d75c086d
DFZ-23 <diego.jaramillo@publicar.com>
2016-12-14 15:22:14 -0500

    cambios index thirdkwnoledge

--- ProveedoresOnLine.IndexSearch/ProveedoresOnLine.IndexSearch.Test/ThirdknowledgeIndexTest.cs
+++ ProveedoresOnLine.IndexSearch/ProveedoresOnLine.IndexSearch.Test/ThirdknowledgeIndexTest.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace ProveedoresOnLine.IndexSearch.Test
 {
-    class ThirdknowledgeIndexTest
+    public class ThirdknowledgeIndexTest
     {
+        
     }
 }
