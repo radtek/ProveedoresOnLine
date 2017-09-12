82d362b1e10fbe901dfcfd85120e4171354dd325
Johann Martinez <johann.martinez@publicar.com>
2017-09-12 11:17:17 -0500

    Calification Model done

--- ProveedoresOnLine.Process/ProveedoresOnLine.Process/Implement/ThirdKnowledgeProcessJob.cs
+++ ProveedoresOnLine.Process/ProveedoresOnLine.Process/Implement/ThirdKnowledgeProcessJob.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Quartz;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProveedoresOnLine.Process.Implement
 {
+    [DisallowConcurrentExecution]
     public class ThirdKnowledgeProcessJob : Quartz.IJob
     {
         public void Execute(Quartz.IJobExecutionContext context)
