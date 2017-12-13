6f98ff12ed75ba924e637dd1f8e1ad6fb08f0fdc
Sergio Alexander Palacios Guevara <sergio.palacios@proveedoresonline.co>
2017-12-13 09:37:16 -0500

    Bug Administrator Roles

--- BackOffice/BackOffice.Web/Scripts/Site/AdminScripts.js
+++ BackOffice/BackOffice.Web/Scripts/Site/AdminScripts.js
@@ -2027,7 +2027,7 @@ var Admin_CompanyRoleObject = {
                             transport: {
                                 read: function (options) {
                                     $.ajax({
-                                        url: BaseUrl.ApiUrl + '/ProviderApi?GetAllCustomers=true&ProviderPublicId=null',
+                                        url: BaseUrl.ApiUrl + '/ProviderApi?GetAllCustomers=true&ProviderPublicId=null' + '&SearchParam=' + options.data.filter.filters[0].value,
                                         dataType: 'json',
                                         success: function (result) {
                                             options.success(result);
